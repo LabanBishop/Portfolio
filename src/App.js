4a1127797d1d670c3806b0d3e84315abce888911
LabanBishop <133487856+LabanBishop@users.noreply.github.com>
2025-04-05 20:41:03 +0100

    now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import picture from './picture.svg';
-import './App.css';
+import picture from "./picture.svg";
+import "./App.css";
+import Navbar from "./Components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div style={{height: "60vh", width:  "10%",}}>
-          <img src={picture} className="App-Profilepic" alt="Picture" />
+      <Navbar />
+      <div className="card mb-3" style={{ width: "fit-content", margin: "1.5% auto" }}>
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src= {picture} class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Portfolio Introduction</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
         </div>
-        <h1>
-          this is my portfolio
-        </h1>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
