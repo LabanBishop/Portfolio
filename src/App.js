62a9808f82ec152afabb8c34cf6dfbaa5423859b
LabanBishop <133487856+LabanBishop@users.noreply.github.com>
2025-04-06 12:33:31 +0100

    w

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import picture from "./picture.svg";
 import "./App.css";
 import Navbar from "./Components/NavBar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  jss: {
+    backgroundColor: "hotpink",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: 5,
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "deeppink",
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <Navbar />
-      <div className="card mb-3" style={{ width: "fit-content", margin: "1.5% auto" }}>
+      <div
+        className="card mb-3"
+        style={{ width: "fit-content", margin: "1.5% auto" }}
+      >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src= {picture} class="img-fluid rounded-start" alt="..." />
+            <img src={picture} class="img-fluid rounded-start" alt="..." />
           </div>
           <div class="col-md-8">
             <div class="card-body">
@@ -23,6 +43,9 @@ function App() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="container text-center mt-5">
+        <button className={`btn ${classes.jss}`}>Click Me</button>
       </div>
     </div>
   );
