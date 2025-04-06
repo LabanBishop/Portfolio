cb6c57b847bb864f284a1b7dea987eb443efea15
LabanBishop <133487856+LabanBishop@users.noreply.github.com>
2025-04-06 13:00:10 +0100

    a

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import "./index.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const greeting = "Hello";
+console.log(greeting); 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
